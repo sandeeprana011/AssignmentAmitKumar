35325610f32a4587d51866179001cd5673bcbfa6
sandeeprana011 <sandeep.rana011@gmail.com>
2017-04-20 01:07:18 +0530

    Added File permission and Some other things

--- app/src/main/java/com/rana/assignment/MainActivity.java
+++ app/src/main/java/com/rana/assignment/MainActivity.java
@@ -1,17 +1,53 @@
 package com.rana.assignment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int READ_REQUEST = 11;
+    private static final int PICKFILE_REQUEST_CODE = 14;
     @BindView(R.id.b_askforfile_mainactivity) Button b_askforfile_mainactivity;
     @BindView(R.id.rv_list_mainactivity) RecyclerView rv_list_mainactivity;
+
+    public static String getPath(final Context context, final Uri uri) {
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +56,46 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.b_askforfile_mainactivity)
+    void onClickAskForFileButton() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("text/*");
+            startActivityForResult(intent, PICKFILE_REQUEST_CODE);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    READ_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case PICKFILE_REQUEST_CODE:
+                String pathString = getPath(this, data.getData());
+                if (pathString != null) {
+                    File file = new File(pathString);
+                    openFileAndShowContent(file);
+                } else {
+                    Toast.makeText(this, "Error : Invalid File Path", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void openFileAndShowContent(File file) {
+        
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case READ_REQUEST:
+                onClickAskForFileButton();
+                break;
+        }
+    }
 }
